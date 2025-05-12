119c3286fa56f9ddbcd9a0b3264915afe9080bf4
sangleb <sanglesada2@gmail.com>
2025-05-12 21:25:21 +0530

    first commit

---
+++ app/(tabs)/Profile.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "@/app/theme";
+
+export default function Profile(){
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>
+                Profile
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.white,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontSize: 20,
+        color: theme.grey,
+        backgroundColor: theme.aquaMarine,
+        fontWeight: "bold",
+    }
+})
---
+++ app/theme.tsx
@@ -0,0 +1,8 @@
+export const theme = {
+    blueViolet: "#8a2be2",
+    aquaMarine:  "#7fffd4",
+    white: "#fff",
+    black: "#000000",
+    grey: "#808080",
+
+}
